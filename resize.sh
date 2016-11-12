fe137bc689c00721df58403a8a4504a695290c7f
redaxmedia <info@redaxmedia.com>
2016-11-12 19:12:29 +0100

    All packs added

--- resize.sh
+++ resize.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-node_modules/svg-resizer/svg-resizer.js -f -x 24 -y 24 --output flaticon build/*.svg
+rm -rf flaticon
+cd build
+
+for path in *
+do
+	mkdir -p ../flaticon/$path;
+done
+
+for path in */*.svg
+do
+	rsvg-convert $path --format svg --keep-aspect-ratio --height 24 --width 24 --output ../flaticon/$path;
+	svgo ../flaticon/$path --pretty --quiet --output ../flaticon/$path;
+	echo -n .;
+done
+echo;
