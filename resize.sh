e669314e5b26a0bae962e2f67d4f4afc8a4d6d05
redaxmedia <info@redaxmedia.com>
2016-11-12 22:58:12 +0100

    Disable convert path data

--- resize.sh
+++ resize.sh
@@ -11,7 +11,7 @@ done
 for path in */*.svg
 do
 	rsvg-convert $path --format svg --keep-aspect-ratio --height 24 --width 24 --output ../flaticon/$path;
-	svgo ../flaticon/$path --pretty --quiet --output ../flaticon/$path;
+	svgo ../flaticon/$path --disable=convertPathData --pretty --quiet --output ../flaticon/$path;
 	echo -n .;
 done
 echo;
